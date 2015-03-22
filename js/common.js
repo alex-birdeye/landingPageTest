c0d23134e097e79679a559d1e4e5fd1bf397440f
root <root@localhost.localdomain>
2015-03-22 20:40:22 +0200

    menu added

--- js/common.js
+++ js/common.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
 
-	$(".main_head").css("min-height", $(window).height());
+	function heightDetect(){
+		$(".main_head").css("height", $(window).height())
+	}
+	heightDetect()
+	$(window).resize(function(){
+		heightDetect()
+	})
+
+	$(".toggle_mnu, .menu_item").click(function() {
+ 		$(".sandwich").toggleClass("active")
+	});
+
+	$(".toggle_mnu").click(function(){
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_mnu").fadeOut(600);
+		} else{
+			$(".top_mnu").fadeIn(600);
+		};
+	});
 	
-});
+})
 $(window).load(function() {
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
-}); 
+	$(".loader_inner").fadeOut()
+	$(".loader").delay(400).fadeOut("slow")
+}) 
