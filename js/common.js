9f015709a4a8d168b9c6df78d8679f5934b555da
youser <youser@localhost.localdomain>
2015-04-08 22:26:49 +0300

    head finished

--- js/common.js
+++ js/common.js
@@ -19,9 +19,11 @@ $(document).ready(function() {
 
 	$(".toggle_mnu").click(function(){
 		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").removeClass("h_opacify");
 			$(".top_mnu").fadeOut(600);
 			$(".top_mnu li a").removeClass("fadeInUp animated");
 		} else{
+			$(".top_text").addClass("h_opacify");
 			$(".top_mnu").fadeIn(600);
 			$(".top_mnu li a").addClass("fadeInUp animated");
 		};
